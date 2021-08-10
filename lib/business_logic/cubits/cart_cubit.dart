a71ffd2467b0c9866c2628a80ab88ad2d6b97bcd
P-Connor <cmpopp1@yahoo.com>
2021-08-10 04:20:06 -0500

    cleaned up cart cubit for future

--- lib/business_logic/cubits/cart_cubit.dart
+++ lib/business_logic/cubits/cart_cubit.dart
@@ -6,15 +6,4 @@ part 'cart_state.dart';
 
 class CartCubit extends Cubit<CartState> {
   CartCubit() : super(CartInitial());
-
-  int get subtotal {
-    int subtotal = 0;
-    for (CartItem item in state.items) {
-      subtotal += item.price * item.quantity;
-    }
-    return subtotal;
-  }
-
-  void clear() {}
-  void add(int itemId, int quantity) {}
 }
