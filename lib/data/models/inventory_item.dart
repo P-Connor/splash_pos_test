ca59b6f0d53639e28cb27c63f840b11daf8e7f91
P-Connor <cmpopp1@yahoo.com>
2021-08-10 04:23:24 -0500

    added default constructor and toString method

--- lib/data/models/inventory_item.dart
+++ lib/data/models/inventory_item.dart
@@ -1,5 +1,23 @@
-class InventoryItem {
-  String? name, displayName, description;
+import 'currency.dart';
 
-  int price = 0;
+class InventoryItem {
+  String name, displayName, description;
+  Currency price;
+  bool nonTax;
+
+  InventoryItem({
+    required this.name,
+    this.displayName = "",
+    this.description = "",
+    this.price = const Currency(0),
+    this.nonTax = false,
+  });
+
+  @override
+  String toString() {
+    return displayName +
+        " - Price: " +
+        price.toString() +
+        (nonTax ? " (NonTaxable)" : "");
+  }
 }
